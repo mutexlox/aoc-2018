071518bcdf55b6b963bbc73cbcb2c2f2aa8284df
Miriam Zimmerman <miriam@mutexlox.com>
2018-12-01 22:23:29 -0800

    Update day01 to solve part 2

--- day01_chronal_calibration.rs
+++ day01_chronal_calibration.rs
@@ -1,15 +1,21 @@
 use std::io::{self, Read};
+use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
     let ints_res : Result<Vec<i32>, _> = input.split_whitespace().map(|s| s.parse::<i32>()).collect();
-    let ints = ints_res.unwrap();
+    let mut ints = ints_res.unwrap().into_iter().cycle();
 
-    let res : i32 = ints.into_iter().sum();
+    let mut sum = 0;
+    let mut seen = HashSet::<i32>::new();
+    while !seen.contains(&sum) {
+        seen.insert(sum);
+        sum += ints.next().unwrap();
+    }
 
-    println!("{}", res);
+    println!("{}", sum);
 
     Ok(())
 }
