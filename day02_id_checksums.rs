10256be89a6098c7561277089bfead931da32970
Miriam Zimmerman <miriam@mutexlox.com>
2018-12-03 22:49:31 -0800

    Add day02 part 2

--- day02_id_checksums.rs
+++ day02_id_checksums.rs
@@ -14,11 +14,31 @@ fn has_letter_n_times(hist : &HashMap<char, i32>, n : i32) -> bool {
     hist.values().any(|x| *x == n)
 }
 
+fn differ_by_1(s1: &str, s2: &str) -> Option<String> {
+    if s1.len() != s2.len() {
+        return None;
+    }
+    let mut found_diff = false;
+    let mut out : String = String::new();
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 == c2 {
+            out.push(c1);
+        } else {
+            if found_diff {
+                return None;
+            }
+            found_diff = true;
+        }
+    }
+    Some(out)
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
     let ids = input.split_whitespace();
+    let ids_vec : Vec<_> = ids.clone().collect();
     let mut num_with_pairs = 0;
     let mut num_with_triples = 0;
     for id in ids {
@@ -31,4 +51,14 @@ fn main() {
         }
     }
     println!("{}", num_with_pairs * num_with_triples);
+    'outer: for i in 0..ids_vec.len() {
+        for j in (i + 1)..ids_vec.len() {
+            if let Some(s) = differ_by_1(ids_vec[i], ids_vec[j]) {
+                println!("{}", s);
+                break 'outer;
+            }
+        }
+    }
+
+
 }
