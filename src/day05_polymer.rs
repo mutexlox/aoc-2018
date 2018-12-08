8acd31d0ef7b08535cecdfaae49993ad77f1f860
Miriam Zimmerman <miriam@mutexlox.com>
2018-12-08 10:54:47 -0800

    Day 05 part 2

--- src/day05_polymer.rs
+++ src/day05_polymer.rs
@@ -1,20 +1,29 @@
 use std::io::{self, Read};
 
-fn is_unit(a: char, b: char) -> bool {
+fn is_reactable(a: char, b: char) -> bool {
     a.to_lowercase().to_string() == b.to_lowercase().to_string() && a != b
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
+    let alpha = "abcdefghijklmnopqrstuvwxyz";
 
-    let mut stack = Vec::new();
-    for c in input.trim().chars() {
-        if stack.len() != 0 && is_unit(stack[stack.len() - 1], c) {
-            stack.pop();
-        } else {
-            stack.push(c);
+    let mut min_len = 0;
+    for remove in alpha.chars() {
+        let mut stack = Vec::new();
+        for c in input.trim().chars() {
+            if c.to_lowercase().to_string() != remove.to_lowercase().to_string() {
+                if stack.len() != 0 && is_reactable(stack[stack.len() - 1], c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        if stack.len() < min_len || remove == 'a' {
+            min_len = stack.len();
         }
     }
-    println!("Remaining: {}", stack.len());
+    println!("Min len: {}", min_len);
 }
