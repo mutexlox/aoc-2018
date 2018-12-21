0948b7c0f75ef0e397fb378929218296b922367c
Miriam Zimmerman <miriam@mutexlox.com>
2018-12-20 21:41:49 -0800

    Day 14 pt 2

--- src/day14_recipe.rs
+++ src/day14_recipe.rs
@@ -1,11 +1,20 @@
 use std::io::{self, Read};
 
-fn add_new_scores(scores: &mut Vec<usize>, elves: &Vec<usize>) {
-    let sum: usize = scores[elves[0]] + scores[elves[1]];
+// Return Some(idx) if haystack is found at idx.
+fn add_new_scores(scores: &mut Vec<u32>, elves: &Vec<usize>, haystack: &[u32]) -> Option<usize> {
+    let sum = scores[elves[0]] + scores[elves[1]];
+    let mut ret = None;
     if sum >= 10 {
         scores.push((sum / 10) % 10);
+        if scores.ends_with(haystack) {
+            ret = Some(scores.len() - haystack.len());
+        }
     }
     scores.push(sum % 10);
+    if scores.ends_with(haystack) {
+        ret = Some(scores.len() - haystack.len());
+    }
+    ret
 }
 
 fn main() {
@@ -13,12 +22,23 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let after = input.trim().parse().unwrap();
 
+    let digits = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+
     let mut scores = vec![3, 7];
     let mut elf_indices = vec![0, 1];
-    while scores.len() < after + 10 {
-        add_new_scores(&mut scores, &elf_indices);
+    loop {
+        //while scores.len() < after + 10 {
+        let idx_opt = add_new_scores(&mut scores, &elf_indices, digits.as_slice());
         for idx in &mut elf_indices {
-            *idx = (*idx + 1 + scores[*idx]) % scores.len();
+            *idx = (*idx + 1 + (scores[*idx] as usize)) % scores.len();
+        }
+        if let Some(idx) = idx_opt {
+            println!("{}", idx);
+            break;
         }
     }
     println!(
