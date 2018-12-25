e843b6cab88e36ee362cbd034620e3b3571fb3da
Miriam Zimmerman <miriam@mutexlox.com>
2018-12-24 17:31:51 -0800

    Day 16 part 2

--- src/day16_interp.rs
+++ src/day16_interp.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Read};
 use std::fmt;
+use std::io::{self, Read};
 
 #[derive(Debug, Clone, Copy)]
 enum Source {
@@ -15,7 +15,11 @@ struct Op {
 
 impl Op {
     fn new(op: Box<dyn Fn(usize, usize) -> usize>, sources: (Source, Source), name: &str) -> Op {
-        Op { op, sources, name: name.to_string() }
+        Op {
+            op,
+            sources,
+            name: name.to_string(),
+        }
     }
 }
 
@@ -44,11 +48,67 @@ fn parse_arr(line: &str) -> Vec<usize> {
         .collect::<Vec<_>>()
 }
 
-// Before: [2, 1, 1, 0]
-// 4 2 1 3
-// After:  [2, 1, 1, 0]
-// like: [10,   12,   15]
-//       gtrr   gtri, eqir
+/// Build a mapping from operators in the input to operators in our order.
+fn get_op_mapping<'a, I>(ops: &Vec<Op>, mut sample_input: I) -> Vec<usize>
+where
+    I: Iterator<Item = &'a str>,
+{
+    // At the start, each number could be any possible op. (Index into |ops|)
+    let mut possible_op_assignment: Vec<Vec<usize>> = vec![(0..16).collect(); 16];
+
+    while let Some(before) = sample_input.next() {
+        if !before.starts_with("Before:") {
+            // Finished this section of input.
+            break;
+        }
+        let regs = parse_arr(before);
+        let op_line = sample_input.next().unwrap();
+        let instr = op_line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+        let after = sample_input.next().unwrap();
+        let after_regs = parse_arr(after);
+
+        let mut i = 0;
+        let mut possible_ops = &mut possible_op_assignment[instr[0]];
+        while i < possible_ops.len() {
+            let op = &ops[possible_ops[i]];
+            let mut tmp_regs = regs.to_vec();
+            do_op(op, &mut tmp_regs, instr[1], instr[2], instr[3]);
+            if after_regs != tmp_regs {
+                possible_ops.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+        // Skip next blank line
+        sample_input.next();
+    }
+
+    // Filter down to unique assignments.
+    let mut unique = possible_op_assignment
+        .iter()
+        .cloned()
+        .filter(|poss| poss.len() == 1)
+        .collect::<Vec<_>>();
+    while unique.len() < 16 {
+        for other in &mut possible_op_assignment {
+            if other.len() > 1 {
+                other.retain(|x| !unique.contains(&vec![*x]));
+            }
+        }
+        unique = possible_op_assignment
+            .iter()
+            .cloned()
+            .filter(|poss| poss.len() == 1)
+            .collect();
+    }
+
+    possible_op_assignment.iter().cloned().flatten().collect()
+}
+
 fn main() {
     // Define base operators.
     let mut ops = Vec::new();
@@ -70,47 +130,27 @@ fn main() {
     }
     assert_eq!(ops.len(), 16);
 
-    // At the start, each number could be any possible op. (Index into |ops|)
-    // let mut possible_op_assignment: Vec<Vec<usize>>= vec![(0..16).collect(); 16];
-
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let mut lines = input.trim().split("\n");
-    let mut like_three = 0;
-    while let Some(before) = lines.next() {
-        if !before.starts_with("Before:") {
-            // Finished this section of input.
-            break;
-        }
-        let regs = parse_arr(before);
-        let op_line = lines.next().unwrap();
-        let instr = op_line
-            .trim()
+    let lines = input.trim().split("\n\n\n\n").collect::<Vec<_>>();
+
+    let op_mapping = get_op_mapping(&ops, lines[0].split("\n"));
+
+    let mut regs = vec![0; 4];
+
+    for instr in lines[1].split("\n") {
+        let parsed_instr: Vec<usize> = instr
             .split_whitespace()
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect::<Vec<_>>();
-        let after = lines.next().unwrap();
-        let after_regs = parse_arr(after);
-
-        let mut i = 0;
-        let mut possible_ops = (0..16).collect::<Vec<_>>(); //&mut possible_op_assignment[instr[0]];
-        while i < possible_ops.len() {
-            let op = &ops[possible_ops[i]];
-            let mut tmp_regs = regs.to_vec();
-            do_op(op, &mut tmp_regs, instr[1], instr[2], instr[3]);
-            if after_regs != tmp_regs {
-                possible_ops.remove(i);
-            } else {
-                i += 1;
-            }
-        }
-        if possible_ops.len() >= 3 {
-            like_three += 1;
-        }
-
-        // Skip next blank line
-        lines.next();
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let op_idx = op_mapping[parsed_instr[0]];
+        do_op(
+            &ops[op_idx],
+            &mut regs,
+            parsed_instr[1],
+            parsed_instr[2],
+            parsed_instr[3],
+        );
     }
-
-    println!("{}", like_three);
+    println!("{}", regs[0]);
 }
